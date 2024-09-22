b9063dc263f537dfb4de25d26c18de05731a34e2
MD-Suhad <ssuhad490@gmail.com>
2024-09-22 23:47:42 +0600

    DSA & Problem Solving Start

--- book-service/src/main/java/bjit/ursa/bookservice/datastructures/stack/IntStack.java
+++ book-service/src/main/java/bjit/ursa/bookservice/datastructures/stack/IntStack.java
@@ -6,23 +6,5 @@ import java.util.NoSuchElementException;
 
 public class IntStack implements Stack<Integer> {
 
-    @Override
-    public int height() {
-        return 0;
-    }
 
-    @Override
-    public Object pop() throws NoSuchElementException {
-        return null;
-    }
-
-    @Override
-    public void push(Object o) {
-
-    }
-
-    @Override
-    public Object top() throws NoSuchElementException {
-        return null;
-    }
 }
