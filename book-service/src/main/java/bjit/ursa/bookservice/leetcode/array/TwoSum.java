66e0c638db8d681b9e7242e7e7c8914ae21c6f67
MD-Suhad <ssuhad490@gmail.com>
2024-09-23 23:55:19 +0600

    array leetcode problem push

---
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcode/array/TwoSum.java
@@ -0,0 +1,30 @@
+package bjit.ursa.bookservice.leetcode.array;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+
+            HashMap<Integer,Integer> sum = new HashMap<>();
+            for(int i = 0; i < nums.length;i++){
+                int k = nums[i];
+                int j = target - k;
+                if(sum.containsKey(j)){
+                    return new int[] {sum.get(j),i};
+                }
+                sum.put(k,i);
+            }
+            return new int[] {};
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
