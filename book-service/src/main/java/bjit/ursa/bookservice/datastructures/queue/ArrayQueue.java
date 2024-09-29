16f65717c064f9b9f0073241c43233ce0c7a7069
MD-Suhad <ssuhad490@gmail.com>
2024-09-29 23:37:20 +0600

    queue data structure implement arrayType

---
+++ book-service/src/main/java/bjit/ursa/bookservice/datastructures/queue/ArrayQueue.java
@@ -0,0 +1,49 @@
+package bjit.ursa.bookservice.datastructures.queue;
+
+public class ArrayQueue implements Queue<T>{
+    private Object[] data;
+    private int front;
+    private int rear;
+
+    public ArrayQueue(int capacity){
+        //ArrayQueue maximum size is data.length-1
+        data = new Object[capacity+1];
+        front = 0;
+        rear = 0;
+    }
+
+    @Override
+    public void offer(T elem){
+        if(isFull()){
+            throw new RuntimeException("Queue is full");
+        }
+        data[rear++] = elem;
+        rear = adjustIndex(rear,data.length);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T poll() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        front = adjustIndex(front,data.length);
+        return (T) data[front++];
+    }
+
+    @Override
+    public int size(){
+        return adjustIndex(rear + data.length - front, data.length);
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return rear == front;
+    }
+    public boolean isFull(){
+        return (front+data.length - rear)% data.length == 1;
+    }
+    public int adjustIndex(int index,int size){
+        return index >= size ? index - size : index;
+    }
+}
