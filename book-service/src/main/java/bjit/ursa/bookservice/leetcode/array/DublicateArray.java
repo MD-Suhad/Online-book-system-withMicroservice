b885623d5c7acf8586f16cea465959472967118a
MD-Suhad <ssuhad490@gmail.com>
2024-10-05 23:52:09 +0600

    leetcode problem add

---
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcode/array/DublicateArray.java
@@ -0,0 +1,17 @@
+package bjit.ursa.bookservice.leetcode.array;
+
+public class DublicateArray {
+
+        public int removeElement(int[] nums, int val) {
+            int index = 0;
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i] != nums[val]){
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+            return index;
+
+        }
+    }
+
