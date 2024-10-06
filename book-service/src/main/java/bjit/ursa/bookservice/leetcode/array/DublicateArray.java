1d18faa23d03ed6e28612bf42886c687fde8cf75
MD-Suhad <ssuhad490@gmail.com>
2024-10-06 18:39:05 +0600

    bug fixed

--- book-service/src/main/java/bjit/ursa/bookservice/leetcode/array/DublicateArray.java
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcode/array/DublicateArray.java
@@ -5,7 +5,7 @@ public class DublicateArray {
         public int removeElement(int[] nums, int val) {
             int index = 0;
             for(int i = 0; i < nums.length; i++){
-                if(nums[i] != nums[val]){
+                if(nums[i] != val){
                     nums[index] = nums[i];
                     index++;
                 }
