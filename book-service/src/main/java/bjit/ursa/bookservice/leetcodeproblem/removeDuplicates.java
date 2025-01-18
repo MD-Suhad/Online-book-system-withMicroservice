066774705ccc5b928343d106328722347fe6fe12
MD-Suhad <ssuhad490@gmail.com>
2025-01-18 23:48:13 +0600

    added leetcode problem

---
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/removeDuplicates.java
@@ -0,0 +1,15 @@
+package bjit.ursa.bookservice.leetcodeproblem;
+
+public class removeDuplicates {
+
+    public int removeDuplicates(int[] nums){
+        int i = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++
+            }
+        }
+        return j;
+    }
+}
