f519967d54467b4017b51388118b4be3c7f3683b
MD-Suhad <ssuhad490@gmail.com>
2025-01-20 23:53:15 +0600

    foursum problem added

--- book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/removeDuplicates.java
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/removeDuplicates.java
@@ -4,7 +4,8 @@ public class removeDuplicates {
 
     public int removeDuplicates(int[] nums){
         int i = 1;
-        for(int i = 1; i < nums.length; i++){
+        for(int
+            i = 1; i < nums.length; i++){
             if(nums[i] != nums[i-1]){
                 nums[j] = nums[i];
                 j++;
