d7b586b0bca86ee716aa275cf2d3a5509a8b721c
MD-Suhad <ssuhad490@gmail.com>
2025-01-24 23:07:07 +0600

    added the problem

---
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/reverseInteger.java
@@ -0,0 +1,18 @@
+package bjit.ursa.bookservice.leetcodeproblem;
+
+public class reverseInteger {
+    public int reverse(int x) {
+        int res = 0;
+        boolean isNegative = x < 0;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+
+        try {
+            res = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+
+        return isNegative ? -res : res;
+    }
+}
