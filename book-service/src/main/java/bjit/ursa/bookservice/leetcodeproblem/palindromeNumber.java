10250e370dfa54d6fdba9a05d7ea9efc68926d30
MD-Suhad <ssuhad490@gmail.com>
2025-02-06 23:49:44 +0600

    added the longestCommonPrefix

--- book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/palindromeNumber.java
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/palindromeNumber.java
@@ -2,25 +2,20 @@ package bjit.ursa.bookservice.leetcodeproblem;
 
 public class palindromeNumber {
     public int reverse(int x) {
-        if(x<0){
+        if (x < 0) {
             return false;
         }
-        if(x<10){
-            return true;
+
+        long reversed = 0;
+        long temp = x;
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
         }
-        if(x % 10 === 0){
-            return false;
-        }
-        int str = String(x);
-        int i = 0;
-        int j = str.length-1;
-        while(i<j){
-            if(str[i] !== str[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+
+        return (reversed == x);
     }
-}
+    }
+
