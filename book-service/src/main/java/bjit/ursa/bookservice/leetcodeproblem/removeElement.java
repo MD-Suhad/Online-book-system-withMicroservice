f5741b0f92b4f4daac5de0a08b9832930e86076f
MD-Suhad <ssuhad490@gmail.com>
2025-02-07 23:27:16 +0600

    added the problem

--- book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/removeElement.java
+++ book-service/src/main/java/bjit/ursa/bookservice/leetcodeproblem/removeElement.java
@@ -4,7 +4,7 @@ public class removeElement {
     public int removeElement(int[] nums, int val){
         int index = 0;
         for(int i = 0; i < nums.length;i++){
-            if(nums != val){
+            if(val != nums){
                 nums[index] = nums[i];
                 index++;
             }
